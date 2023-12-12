d62238b922ad289689c0dd9fa2daf4da4606ccf6
Ulli Hafner <ullrich.hafner@gmail.com>
2023-12-12 12:18:44 +0100

    Fix building of paths on Windows.

--- src/main/java/edu/hm/hafner/grading/CommentBuilder.java
+++ src/main/java/edu/hm/hafner/grading/CommentBuilder.java
@@ -2,6 +2,7 @@ package edu.hm.hafner.grading;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map.Entry;
@@ -16,6 +17,7 @@ import edu.hm.hafner.coverage.Metric;
 import edu.hm.hafner.coverage.Mutation;
 import edu.hm.hafner.util.FilteredLog;
 import edu.hm.hafner.util.LineRange;
+import edu.hm.hafner.util.PathUtil;
 
 /**
  * Creates comments for static analysis warnings, for lines with missing coverage, and for lines with survived
@@ -24,24 +26,27 @@ import edu.hm.hafner.util.LineRange;
  * @author Ullrich Hafner
  */
 public abstract class CommentBuilder {
-    private static final String GITHUB_WORKSPACE_REL = "/github/workspace/./";
-    private static final String GITHUB_WORKSPACE_ABS = "/github/workspace/";
     private static final int NO_COLUMN = -1;
     private static final String NO_ADDITIONAL_DETAILS = StringUtils.EMPTY;
+    private static final PathUtil PATH_UTIL = new PathUtil();
+
+    private final List<String> prefixes;
+
+    protected CommentBuilder(final String... prefixesToRemove) {
+        prefixes = Arrays.asList(prefixesToRemove);
+    }
 
     void createAnnotations(final AggregatedScore score, final FilteredLog log) {
         if (getEnv("SKIP_ANNOTATIONS", log).isEmpty()) {
-            var prefix = computeAbsolutePathPrefixToRemove(log);
-
             var additionalAnalysisSourcePaths = extractAdditionalSourcePaths(score.getAnalysisScores());
-            createAnnotationsForIssues(score, prefix, additionalAnalysisSourcePaths);
+            createAnnotationsForIssues(score, additionalAnalysisSourcePaths);
 
             var additionalCoverageSourcePaths = extractAdditionalSourcePaths(score.getCodeCoverageScores());
-            createAnnotationsForMissedLines(score, prefix, additionalCoverageSourcePaths);
-            createAnnotationsForPartiallyCoveredLines(score, prefix, additionalCoverageSourcePaths);
+            createAnnotationsForMissedLines(score, additionalCoverageSourcePaths);
+            createAnnotationsForPartiallyCoveredLines(score, additionalCoverageSourcePaths);
 
             var additionalMutationSourcePaths = extractAdditionalSourcePaths(score.getMutationCoverageScores());
-            createAnnotationsForSurvivedMutations(score, prefix, additionalMutationSourcePaths);
+            createAnnotationsForSurvivedMutations(score, additionalMutationSourcePaths);
         }
     }
 
@@ -72,11 +77,6 @@ public abstract class CommentBuilder {
             int columnStart, int columnEnd,
             String details);
 
-    private String computeAbsolutePathPrefixToRemove(final FilteredLog log) {
-        return String.format("%s/%s/", getEnv("RUNNER_WORKSPACE", log),
-                StringUtils.substringAfter(getEnv("GITHUB_REPOSITORY", log), "/"));
-    }
-
     private Set<String> extractAdditionalSourcePaths(final List<? extends Score<?, ?>> scores) {
         return scores.stream()
                 .map(Score::getConfiguration)
@@ -86,35 +86,40 @@ public abstract class CommentBuilder {
     }
 
     private void createAnnotationsForIssues(final AggregatedScore score,
-            final String prefix, final Set<String> sourcePaths) {
-        score.getIssues().forEach(issue -> createAnnotationForIssue(issue, prefix, sourcePaths));
+            final Set<String> sourcePaths) {
+        score.getIssues().forEach(issue -> createAnnotationForIssue(issue, sourcePaths));
     }
 
     private void createAnnotationForIssue(final Issue issue,
-            final String prefix, final Set<String> sourcePaths) {
-        var path = createRelativeRepositoryPath(issue.getFileName(), prefix, sourcePaths);
-        var relativePath = StringUtils.removeStart(
-                StringUtils.removeStart(path, GITHUB_WORKSPACE_REL), GITHUB_WORKSPACE_ABS);
+            final Set<String> sourcePaths) {
+        var relativePath = cleanPath(createRelativeRepositoryPath(issue.getFileName(), sourcePaths));
 
         createComment(relativePath, issue.getLineStart(), issue.getLineEnd(), issue.getMessage(),
                 issue.getOriginName() + ": " + issue.getType(), issue.getColumnStart(), issue.getColumnEnd(),
                 NO_ADDITIONAL_DETAILS);
     }
 
-    private void createAnnotationsForMissedLines(final AggregatedScore score,
-            final String prefix, final Set<String> sourcePaths) {
-        score.getCoveredFiles(Metric.LINE).forEach(file -> createAnnotationsForMissedLines(file, prefix, sourcePaths));
+    private String cleanPath(final String path) {
+        for (String prefix : prefixes) {
+            if (path.startsWith(prefix)) {
+                return StringUtils.removeStart(path, prefix);
+            }
+        }
+        return path;
     }
 
-    private void createAnnotationsForMissedLines(final FileNode file,
-            final String prefix, final Set<String> sourcePaths) {
+    private void createAnnotationsForMissedLines(final AggregatedScore score, final Set<String> sourcePaths) {
+        score.getCoveredFiles(Metric.LINE).forEach(file -> createAnnotationsForMissedLines(file, sourcePaths));
+    }
+
+    private void createAnnotationsForMissedLines(final FileNode file, final Set<String> sourcePaths) {
         file.getMissedLineRanges()
-                .forEach(range -> createAnnotationForMissedLineRange(file, range, prefix, sourcePaths));
+                .forEach(range -> createAnnotationForMissedLineRange(file, range, sourcePaths));
     }
 
     private void createAnnotationForMissedLineRange(final FileNode file, final LineRange range,
-            final String prefix, final Set<String> sourcePaths) {
-        var relativePath = createRelativeRepositoryPath(file.getRelativePath(), prefix, sourcePaths);
+            final Set<String> sourcePaths) {
+        var relativePath = createRelativeRepositoryPath(file.getRelativePath(), sourcePaths);
 
         createComment(relativePath,
                 range.getStart(), range.getEnd(),
@@ -138,21 +143,21 @@ public abstract class CommentBuilder {
     }
 
     private void createAnnotationsForPartiallyCoveredLines(final AggregatedScore score,
-            final String prefix, final Set<String> sourcePaths) {
+            final Set<String> sourcePaths) {
         score.getCoveredFiles(Metric.BRANCH)
-                .forEach(file -> createAnnotationsForMissedBranches(file, prefix, sourcePaths));
+                .forEach(file -> createAnnotationsForMissedBranches(file, sourcePaths));
     }
 
     private void createAnnotationsForMissedBranches(final FileNode file,
-            final String prefix, final Set<String> sourcePaths) {
+            final Set<String> sourcePaths) {
         file.getPartiallyCoveredLines().entrySet()
-                .forEach(entry -> createAnnotationForMissedBranches(file, entry, prefix, sourcePaths));
+                .forEach(entry -> createAnnotationForMissedBranches(file, entry, sourcePaths));
     }
 
     private void createAnnotationForMissedBranches(final FileNode file,
             final Entry<Integer, Integer> branchCoverage,
-            final String prefix, final Set<String> sourcePaths) {
-        createComment(createRelativeRepositoryPath(file.getRelativePath(), prefix, sourcePaths),
+            final Set<String> sourcePaths) {
+        createComment(createRelativeRepositoryPath(file.getRelativePath(), sourcePaths),
                 branchCoverage.getKey(), branchCoverage.getKey(),
                 createBranchMessage(branchCoverage.getKey(), branchCoverage.getValue()), "Partially covered line",
                 NO_COLUMN, NO_COLUMN, NO_ADDITIONAL_DETAILS);
@@ -165,14 +170,13 @@ public abstract class CommentBuilder {
         return String.format("Line %d is only partially covered, %d branches are missing", line, missed);
     }
 
-    private String createRelativeRepositoryPath(final String fileName,
-            final String prefix, final Set<String> sourcePaths) {
-        var cleaned = StringUtils.removeStart(fileName, prefix);
+    private String createRelativeRepositoryPath(final String fileName, final Set<String> sourcePaths) {
+        var cleaned = cleanPath(fileName);
         if (Files.exists(Path.of(cleaned))) {
             return cleaned;
         }
         for (String s : sourcePaths) {
-            var added = s + "/" + cleaned;
+            var added = PATH_UTIL.createAbsolutePath(s, cleaned);
             if (Files.exists(Path.of(added))) {
                 return added;
             }
@@ -181,21 +185,21 @@ public abstract class CommentBuilder {
     }
 
     private void createAnnotationsForSurvivedMutations(final AggregatedScore score,
-            final String prefix, final Set<String> sourcePaths) {
+            final Set<String> sourcePaths) {
         score.getCoveredFiles(Metric.MUTATION)
-                .forEach(file -> createAnnotationsForSurvivedMutations(file, prefix, sourcePaths));
+                .forEach(file -> createAnnotationsForSurvivedMutations(file, sourcePaths));
     }
 
     private void createAnnotationsForSurvivedMutations(final FileNode file,
-            final String prefix, final Set<String> sourcePaths) {
+            final Set<String> sourcePaths) {
         file.getSurvivedMutationsPerLine().entrySet()
-                .forEach(entry -> createAnnotationForSurvivedMutation(file, entry, prefix, sourcePaths));
+                .forEach(entry -> createAnnotationForSurvivedMutation(file, entry, sourcePaths));
     }
 
     private void createAnnotationForSurvivedMutation(final FileNode file,
             final Entry<Integer, List<Mutation>> mutationsPerLine,
-            final String prefix, final Set<String> sourcePaths) {
-        createComment(createRelativeRepositoryPath(file.getRelativePath(), prefix, sourcePaths),
+            final Set<String> sourcePaths) {
+        createComment(createRelativeRepositoryPath(file.getRelativePath(), sourcePaths),
                 mutationsPerLine.getKey(), mutationsPerLine.getKey(),
                 createMutationMessage(mutationsPerLine.getKey(), mutationsPerLine.getValue()), "Mutation survived",
                 NO_COLUMN, NO_COLUMN, createMutationDetails(mutationsPerLine.getValue()));
