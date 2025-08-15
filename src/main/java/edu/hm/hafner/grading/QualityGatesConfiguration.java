eefdc46b1b66be56acebd8d3ec87214b389bc571
Ulli Hafner <ullrich.hafner@gmail.com>
2025-08-15 14:23:46 +0200

    Fix toString name.

--- src/main/java/edu/hm/hafner/grading/QualityGatesConfiguration.java
+++ src/main/java/edu/hm/hafner/grading/QualityGatesConfiguration.java
@@ -179,7 +179,7 @@ public final class QualityGatesConfiguration {
         @Override
         @Generated
         public String toString() {
-            return String.format("QualityGatesConfiguration{metric='%s', threshold=%.2f, criticality='%s'}",
+            return String.format("QualityGateDto{metric='%s', threshold=%.2f, criticality='%s'}",
                     metric, threshold, criticality);
         }
     }
