daacea15ec2bf17bafd7241298426a20c119d585
Ulli Hafner <ullrich.hafner@gmail.com>
2026-03-03 11:06:13 +0100

    Improve rendering of analysis summary

--- src/main/java/edu/hm/hafner/grading/AnalysisMarkdown.java
+++ src/main/java/edu/hm/hafner/grading/AnalysisMarkdown.java
@@ -1,5 +1,7 @@
 package edu.hm.hafner.grading;
 
+import org.apache.commons.lang3.StringUtils;
+
 import edu.hm.hafner.analysis.ReportFormatter;
 import edu.hm.hafner.analysis.registry.ParserRegistry;
 import edu.hm.hafner.grading.TruncatedString.TruncatedStringBuilder;
@@ -36,23 +38,13 @@ public class AnalysisMarkdown extends ScoreMarkdown<AnalysisScore, AnalysisConfi
     @Override
     String createScoreSummary(final AnalysisScore score) {
         var report = score.getReport();
-        if (score.hasDelta()) {
-            if (score.isEmpty()) {
-                return format("%s %s",
-                        FORMATTER.formatSizeOfElements(report),
-                        delta(score.getTotalSizeDelta(), true));
-            }
-            return format("%s %s &mdash; %s",
-                    FORMATTER.formatSizeOfElements(report),
-                    delta(score.getTotalSizeDelta(), true),
-                    FORMATTER.formatSeverities(report));
-        }
+
+        var title = FORMATTER.formatSizeOfElements(report);
+        var delta = score.hasDelta() ? " " + delta(score.getTotalSizeDelta(), true) : StringUtils.EMPTY;
         if (score.isEmpty()) {
-            return FORMATTER.formatSizeOfElements(report);
+            return title + delta;
         }
-        return format("%s (%s)",
-                FORMATTER.formatSizeOfElements(report),
-                FORMATTER.formatSeverities(report));
+        return title + delta + " &mdash; " + FORMATTER.formatSeverities(report);
     }
 
     @Override
