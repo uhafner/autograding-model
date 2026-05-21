876d7f36924cf2b7197606f7d67dcff38b1da33d
Ulli Hafner <ullrich.hafner@gmail.com>
2026-05-21 22:37:32 +0200

    Improve coverage

--- src/test/java/edu/hm/hafner/grading/GradingReportTest.java
+++ src/test/java/edu/hm/hafner/grading/GradingReportTest.java
@@ -203,7 +203,50 @@ class GradingReportTest {
     @Test
     @SetEnvironmentVariable(key = "COMMIT_URL", value = "invalid-url")
     @SetEnvironmentVariable(key = "RUN_URL", value = "https://github.com/uhafner/autograding-model/actions/runs/1")
-    void shouldNotCreateLinksForInvalidUrls() {
+    void shouldNotCreateLinksForInvalidCommitUrl() {
+        var results = new GradingReport();
+
+        var score = AggregatedScoreTest.createQualityAggregation();
+
+        assertThatReferenceIsMissing(results, score);
+    }
+
+    @Test
+    @SetEnvironmentVariable(key = "RUN_URL", value = "invalid-url")
+    @SetEnvironmentVariable(key = "COMMIT_URL", value = "https://github.com/uhafner/autograding-model/actions/runs/1")
+    void shouldNotCreateLinksForInvalidRunUrls() {
+        var results = new GradingReport();
+
+        var score = AggregatedScoreTest.createQualityAggregation();
+
+        assertThatReferenceIsMissing(results, score);
+    }
+
+    @Test
+    @SetEnvironmentVariable(key = "COMMIT_URL", value = "")
+    @SetEnvironmentVariable(key = "RUN_URL", value = "https://github.com/uhafner/autograding-model/actions/runs/1")
+    void shouldNotCreateLinksForEmptyCommitUrl() {
+        var results = new GradingReport();
+
+        var score = AggregatedScoreTest.createQualityAggregation();
+
+        assertThatReferenceIsMissing(results, score);
+    }
+
+    @Test
+    @SetEnvironmentVariable(key = "COMMIT_URL", value = "https://github.com/uhafner/autograding-model/commit/1234567890")
+    void shouldNotCreateLinksForMissingRunUrl() {
+        var results = new GradingReport();
+
+        var score = AggregatedScoreTest.createQualityAggregation();
+
+        assertThatReferenceIsMissing(results, score);
+    }
+
+    @Test
+    @SetEnvironmentVariable(key = "COMMIT_URL", value = "https://github.com/uhafner/autograding-model/commit/1234567890")
+    @SetEnvironmentVariable(key = "RUN_URL", value = "")
+    void shouldNotCreateLinksForEmptyRunUrl() {
         var results = new GradingReport();
 
         var score = AggregatedScoreTest.createQualityAggregation();
@@ -213,9 +256,9 @@ class GradingReportTest {
 
     private void assertThatReferenceIsMissing(final GradingReport results, final AggregatedScore score) {
         assertThat(results.getMarkdownSummary(score, "Summary"))
-                .doesNotContain("## :pushpin: Reference Results", "[Commit]", "[Run]");
+                .doesNotContain("## :pushpin: Reference Results");
         assertThat(results.getMarkdownDetails(score, "Summary"))
-                .doesNotContain("## :pushpin: Reference Results", "[Commit]", "[Run]");
+                .doesNotContain("## :pushpin: Reference Results");
     }
 
     @Test
